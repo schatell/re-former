c0fa87ec249731a2722899de91d639273f046cda
schatell <sebastien.chatelle.blanchard@gmail.com>
2017-09-15 21:53:12 -0400

    Completed project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -12,8 +13,23 @@ class UsersController < ApplicationController
     end
   end
 
-end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-def user_params
-  params.require(:user).permit(:username,:email,:password)
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
-class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+
+class User < ActiveRecord::Base
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+	validates :password, presence: true
 end
